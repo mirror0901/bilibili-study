65cdca399a158875437de7b7f6c732228751713c
mirror0901 <1755496180@qq.com>
2020-04-22 02:18:43 +0800

    #202004220217 1. apollo-quick-start的简单案例

---
+++ apollo-quick-start/src/main/java/com/mirror95/apollo/GetConfigTest.java
@@ -0,0 +1,24 @@
+package com.mirror95.apollo;
+
+import com.ctrip.framework.apollo.Config;
+import com.ctrip.framework.apollo.ConfigService;
+
+/**
+ * @author: mirror_huang
+ * @qq: 1755496180
+ * @description: 获取apollo的配置
+ * @create: 2020-04-22 02:02
+ **/
+public class GetConfigTest {
+    public static void main(String[] args) {
+        //需要在vm options 配置 -Dapp.id=yj-safe-server -Denv=DEV -Ddev_meta=http://192.168.95.23:8080
+        Config appConfig = ConfigService.getAppConfig();
+        //获取配置信息
+        String property = appConfig.getProperty("sms.enable", null);
+        String propertyName = appConfig.getProperty("name", null);
+        String propertyUnExist = appConfig.getProperty("unExist", null);
+        System.out.println(property);
+        System.out.println(propertyName);
+        System.out.println(propertyUnExist);
+    }
+}
