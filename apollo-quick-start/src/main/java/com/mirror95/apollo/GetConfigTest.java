4370326d6d17e678796bea975892732801ec5533
mirror0901 <1755496180@qq.com>
2020-04-24 01:48:37 +0800

    #2020042230145 1. springboot整合apollo的简单案例    添加集群，命名空间，环境的配置

--- apollo-quick-start/src/main/java/com/mirror95/apollo/GetConfigTest.java
+++ apollo-quick-start/src/main/java/com/mirror95/apollo/GetConfigTest.java
@@ -11,7 +11,7 @@ import com.ctrip.framework.apollo.ConfigService;
  **/
 public class GetConfigTest {
     public static void main(String[] args) {
-        //需要在vm options 配置 -Dapp.id=yj-safe-server -Denv=DEV -Ddev_meta=http://192.168.95.23:8080 -Dapollo.cluster=jiangsu
+        //需要在vm options 配置 -Dapp.id=yj-safe-server -Denv=DEV -Ddev_meta=http://192.168.95.23:8080 -Dapollo.cluster=jiangsu -Dapollo.cacheDir=/opt/data/apollo-config
         Config appConfig = ConfigService.getAppConfig();//获取默认namespace下的配置信息
         //ConfigService.getConfig("");//读取指定namespace下的配置信息
         //获取配置信息
